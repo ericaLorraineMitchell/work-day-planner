b9a446e52e16208f1bf14ab711dad830b39e483c
Lorraine Mitchell <elorrainemitchell@gmail.com>
2022-01-31 14:53:04 -0500

    linking issue push

--- assets/js/jQuery.js
+++ assets/js/jQuery.js
@@ -3,5 +3,8 @@ var currentDisplayEl = document.getElementById("current-display");
 function displayCurrent() {
   var rightNow = moment().format("MMM DD, YYYY [at] hh:mm:ss a");
   console.log(moment().format("MMM DD, YYYY [at] hh:mm:ss a"));
+  console.log("linked");
   currentDisplayEl.textContent(rightNow);
+
+  displayCurrent();
 }
