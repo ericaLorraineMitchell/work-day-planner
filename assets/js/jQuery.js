a5744243adc2abc127461c4a10914d6d1839464f
Lorraine Mitchell <elorrainemitchell@gmail.com>
2022-01-31 17:42:44 -0500

    html time blocks

--- assets/js/jQuery.js
+++ assets/js/jQuery.js
@@ -1,10 +1,11 @@
 var currentDisplayEl = document.getElementById("current-display");
 
+// DISPLAYS CURRENT DATE AND TIME
 function displayCurrent() {
   var rightNow = moment().format("MMM DD, YYYY [at] hh:mm:ss a");
-  console.log(moment().format("MMM DD, YYYY [at] hh:mm:ss a"));
-  console.log("linked");
-  currentDisplayEl.textContent(rightNow);
-
-  displayCurrent();
+  currentDisplayEl.textContent = rightNow;
 }
+
+//CALL FUNCTION & SET TIME INTERVAL TO COUNT SECONDS
+displayCurrent();
+setInterval(displayCurrent, 1000);
