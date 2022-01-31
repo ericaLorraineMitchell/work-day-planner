75be9e55df5a756150ff58c3d81b22a930b1689c
Lorraine Mitchell <elorrainemitchell@gmail.com>
2022-01-31 14:17:45 -0500

    moment.js stamp

---
+++ assets/js/jQuery.js
@@ -0,0 +1,7 @@
+var currentDisplayEl = document.getElementById("current-display");
+
+function displayCurrent() {
+  var rightNow = moment().format("MMM DD, YYYY [at] hh:mm:ss a");
+  console.log(moment().format("MMM DD, YYYY [at] hh:mm:ss a"));
+  currentDisplayEl.textContent(rightNow);
+}
