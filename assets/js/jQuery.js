eed9da8e705867da72ea99fb87a79e4995cd5a65
Lorraine Mitchell <elorrainemitchell@gmail.com>
2022-01-31 22:07:08 -0500

    js work

--- assets/js/jQuery.js
+++ assets/js/jQuery.js
@@ -1,4 +1,5 @@
 var currentDisplayEl = document.getElementById("current-display");
+var savedEvent = document.getElementsByClassName("description");
 
 // DISPLAYS CURRENT DATE AND TIME
 function displayCurrent() {
@@ -9,3 +10,12 @@ function displayCurrent() {
 //CALL FUNCTION & SET TIME INTERVAL TO COUNT SECONDS
 displayCurrent();
 setInterval(displayCurrent, 1000);
+
+//SAVE EVENT TO LOCAL STORAGE & PREVENT DEFAULT RESET
+$(".saveBtn").on("click", function (e) {
+  console.log(e.currentTarget.className);
+//   $(e.target).parent().find("input").val();
+//   localStorage.setItem("event", savedEvent);
+});
+
+//ON SAVE OF EVENT COLOR CODE BASED UPON CURRENT TIME
